c2ef3c3ba425e1ff848e195bed398552e5d75725
Gaurav Chaudhari <101353771+gauravv1311@users.noreply.github.com>
2022-04-07 16:45:07 +0530

    s

--- src/Form.js
+++ src/Form.js
@@ -1,47 +1,51 @@
-import React, {Component} from 'react';
-import './Form.css'
+import React, { Component } from "react";
+import "./Form.css";
 
-class Form extends Component{
-constructor(props){
-  super(props)
-  this.state={
-    username:'',
-    data1: []
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      data1: []
+    };
   }
-}
 
+  handleUsername = (event) => {
+    this.setState({
+      username: event.target.value
+    });
+  };
 
-handleUsername=event=>{
-  this.setState({
-      username:event.target.value
-  })
-}
+  handleSubmit = (e) => {
+    console.log(e);
+    e.preventDefault();
+    this.updateFromDB();
+  };
+  updateFromDB() {
+    fetch(`https://api.github.com/users/${this.username}/repos`, {
+      method: "get"
+    })
+      .then((response) => response.json())
+      .then((data) => this.setState({ data1: data.data1 }));
+  }
 
-handleSubmit=e=>{
-  console.log(e);
-  e.preventDefault();
-  this.updateFromDB();
-}
-updateFromDB(){
-  fetch(`https://api.github.com/users/${this.username}/repos`,{method:'get'})
-  .then(response => response.json())
-  .then(data => this.setState({ data1: data.data1 }));
-}
+  render() {
+    return (
+      <div className="form">
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <label>
+            UserName:
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={(e) => this.handleUsername(e)}
+            />
+          </label>
 
-render(){
-  return(
-    <div className= "form">
-    <Form onSubmit = {(e) => this.handleSubmit(e)}>
-      
-        <label>UserName:
-        <input type='text' value={this.state.username}
-        onChange={this.handleUsername} />
-        </label>
-      
-      <button type='submit'>Submit</button>
-    </Form>
-    </div>
-  )
-}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 export default Form;
